5c3feabd5fff1d6c6a636da5189bfeeaa3710f64
Manavk2004 <mjkamdar04@gmail.com>
2025-08-13 09:48:23 -0400

    Cleaned up unnecessary code, added navbar

--- frontend/Cloud/src/App.jsx
+++ frontend/Cloud/src/App.jsx
@@ -8,6 +8,19 @@ function App() {
   return(
     <>
       <div id="main-background">
+        <nav id="navbar">
+          <div id="navbar-div">
+            <div id='div1'>
+              <li>Home</li>
+            </div>
+            <div id='div2'>
+              <li>My Tickets</li>
+            </div>
+            <div id='div3'>
+              <li>Find Flight</li>
+            </div>
+          </div>
+        </nav>
         <div id="left-side-main-background">
           <div id='top-section-left'>
             {/* <div id="section-left-side">
